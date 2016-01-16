c720c9c23755e0d427df8c8a76bca364c3d5f2c2
prashanthavvari <prashanthavvari@missionrnd.com>
2016-01-16 22:26:48 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -12,7 +12,50 @@ ERROR CASES: Return -1 for invalid inputs.
 
 NOTES:
 */
-
-int findSingleOccurenceNumber(int *A, int len) {
+#include<stdio.h>
+int findSingleOccurenceNumber(int *a, int n) {
+	if (a != 0 && a != NULL&&n>0)
+	{
+		int i, j, count = 0;
+		for (i = 0; i < n; i++)
+		{
+			for (j = i; j < n; j++)
+			{
+				if (a[i]>a[j])
+				{
+					a[i] = a[i] + a[j];
+					a[j] = a[i] - a[j];
+					a[i] = a[i] - a[j];
+				}
+			}
+		}
+		for (i = 0; i < n; i++)
+		{
+			if (a[i - 1] > 0 && a[i + 1] < n)
+			{
+				if (a[i] != a[i + 1] && a[i] != a[i - 1])
+				{
+					return a[i];
+					break;
+				}
+			}
+			else if (a[i - 1] < 0)
+			{
+				if (a[i] != a[i + 1])
+				{
+					return a[i];
+					break;
+				}
+			}
+			else if (a[i + 1] > n)
+			{
+				if (a[i] != a[i - 1])
+				{
+					return a[i];
+					break;
+				}
+			}
+		}
+	}
 	return -1;
 }
